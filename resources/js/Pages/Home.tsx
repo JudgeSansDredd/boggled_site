55531ce9c68f86b7f39d1b80ac10cb5b942ddc01
Nathan Stanley <chemapfours@gmail.com>
2022-11-27 17:54:44 -0500

    Big Bang

---
+++ resources/js/Pages/Home.tsx
@@ -0,0 +1,11 @@
+import { Head } from "@inertiajs/inertia-react";
+import React from "react";
+
+export default function Home() {
+    return (
+        <>
+            <Head title="Home" />
+            <div>This is the home page</div>
+        </>
+    );
+}
